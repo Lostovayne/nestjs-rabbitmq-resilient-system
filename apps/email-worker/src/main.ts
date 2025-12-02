9e431cf4aa2e6ab3e521263d97011fda301a3a01
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2025-12-02 00:45:16 -0300

    chore: remove pnpm-workspace.yaml configuration file

--- apps/email-worker/src/main.ts
+++ apps/email-worker/src/main.ts
@@ -6,15 +6,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Creamos un contexto temporal solo para leer el .env
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+
+  // 2. LEER VARIABLES (Validando que existan)
+  const rmqUrl = configService.getOrThrow<string>('RABBITMQ_URI');
+  const queueName = configService.getOrThrow<string>('RABBITMQ_QUEUE');
+
+  // Evitar gastar ram!
+  await appContext.close();
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://user:password@localhost:5672'],
-        queue: 'notifications_queue', // Misma cola que el producer
+        urls: [rmqUrl],
+        queue: queueName, // Misma cola que el producer
         noAck: false, // <--- IMPORTANTE: Desactivamos auto-confirmación
         queueOptions: {
           durable: true,
@@ -25,6 +37,6 @@ async function bootstrap() {
   );
 
   await app.listen();
-  console.log('🚀 [Worker] Email Service escuchando RabbitMQ v4...');
+  console.log(`🚀 [Worker] Escuchando en ${queueName} (Configurado vía .env)`);
 }
 bootstrap();
